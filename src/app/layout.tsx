eb6ba60d26193e3adc76271c82a4845354edc7b2
Plan C <hubenchang0515@outlook.com>
2025-04-11 05:36:35 +0800

    修复图标错误

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,13 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import '@fontsource/roboto/900.css';
+import nextConfig from "../../next.config";
 
 export const metadata: Metadata = {
   title: "Primers - 编程伙伴",
   description: "欢迎来到 Primers ———— 你的编程学习伙伴。Primers 致力于为各类编程学习者提供全面、系统的编程教程和实践资源。无论你是编程新手，还是有一定基础的开发者，Primers 都提供了适合的学习路径和丰富的实战项目，帮助你从零开始，逐步掌握编程技能，成为一名优秀的开发者。",
   icons: {
-    icon: 'favicon.svg'
+    icon: `${nextConfig.basePath}/icon.svg`,
   }
 };
 
