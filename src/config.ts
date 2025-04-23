050f6948fc3caffb139d5f3d60b0a7722af3b974
Plan C <hubenchang0515@outlook.com>
2025-04-23 19:21:45 +0800

    文档路径可配置

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,7 @@ export const SITE_CONFIG = {
     origin: 'https://xplanc.org',
     basePath: '/primers',
 }
+
+export const DOCUMENT_CONFIG = {
+    root: 'primers-document'
+}
--- src/utils/document.ts
+++ src/utils/document.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import fs from 'fs/promises';
-
-const DOCUMENT_DIR = "primers-document/document"
+import { DOCUMENT_CONFIG } from "@/config";
 
 export function title(filename:string) {
     if (filename.includes(".")) {
@@ -12,31 +11,31 @@ export function title(filename:string) {
 }
 
 export async function languages() {
-    const dir = path.join(process.cwd(), DOCUMENT_DIR);
+    const dir = path.join(process.cwd(), DOCUMENT_CONFIG.root, 'document');
     const files = await fs.readdir(dir, { withFileTypes: true });
     return files.filter(file=>file.isDirectory()).map(file=>file.name);
 }
 
 export async function categories(lang:string) {
-    const dir = path.join(process.cwd(), DOCUMENT_DIR, lang);
+    const dir = path.join(process.cwd(), DOCUMENT_CONFIG.root, 'document', lang);
     const files = await fs.readdir(dir, { withFileTypes: true });
     return files.filter(file=>file.isDirectory()).map(file=>file.name);
 }
 
 export async function chapters(lang:string, category:string) {
-    const dir = path.join(process.cwd(), DOCUMENT_DIR, lang, category);
+    const dir = path.join(process.cwd(), DOCUMENT_CONFIG.root, 'document', lang, category);
     const files = await fs.readdir(dir, { withFileTypes: true });
     return files.filter(file=>file.isDirectory()).map(file=>file.name);
 }
 
 export async function docs(lang:string, category:string, chapter:string) {
-    const dir = path.join(process.cwd(), DOCUMENT_DIR, lang, category, chapter);
+    const dir = path.join(process.cwd(), DOCUMENT_CONFIG.root, 'document', lang, category, chapter);
     const files = await fs.readdir(dir, { withFileTypes: true });
     return files.filter(file=>file.isFile()).map(file=>file.name)
 }
 
 export async function content(lang:string, category:string, chapter?:string, doc?:string) {
-    const file = path.join(process.cwd(), DOCUMENT_DIR, lang, category, chapter??"", doc??"");
+    const file = path.join(process.cwd(), DOCUMENT_CONFIG.root, 'document', lang, category, chapter??"", doc??"");
     const text = await fs.readFile(file, 'utf-8');
     return text;
 }
