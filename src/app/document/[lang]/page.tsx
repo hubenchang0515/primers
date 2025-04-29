4657d4ca0d6f508822684c53d3aed3c5376662b0
Plan C <hubenchang0515@outlook.com>
2025-04-29 22:34:40 +0800

    生成站点地图

---
+++ src/app/document/[lang]/page.tsx
@@ -0,0 +1,62 @@
+import { Content } from "@/components/Content";
+import MainPage from "@/components/MainPage";
+import { TitleBarItem } from "@/components/TitleBar";
+import { SITE_CONFIG } from "@/config";
+import { categories, content, languages, docState, title } from "@/utils/document";
+import { Container, Fade, Paper } from "@mui/material";
+import { Metadata } from "next";
+
+export interface PageParams {
+    lang:string;        // 语言： 例如 中文（zh）、英文（en）
+}
+
+// 生成静态页面路径
+export async function generateStaticParams() {
+    const paramsList:PageParams[] = []
+    for (const lang of await languages()) {
+        paramsList.push({
+            lang: lang,
+        });
+    }
+    return paramsList;
+}
+
+// 生成元数据
+export async function generateMetadata({params}:{params:Promise<PageParams>}): Promise<Metadata> {
+    const path = (await params);
+    const markdown = await content(decodeURIComponent(path.lang), "00.index.md");
+    
+    return {
+        title: `Primers 编程伙伴`,
+        description: markdown.replace(/\n+/g, '').substring(0, 150),
+        icons: {
+            icon: `${SITE_CONFIG.basePath}/icon.svg`,
+        }
+    };
+}
+
+// 生成页面
+export default async function Pastatege({params}:{params:Promise<PageParams>}) {
+    const path = (await params);
+    const markdown = await content(path.lang, "00.index.md");
+    const state = await docState(path.lang, "00.index.md");
+
+    const titleItems:TitleBarItem[] = (await categories(decodeURIComponent(path.lang))).map((item) => {
+        return {
+            label: title(item),
+            url: `/document/${path.lang}/${item}`,
+        }
+    })
+
+    return (
+        <MainPage depth={2} titleItems={titleItems}>
+            <Container maxWidth='lg' sx={{padding:1, width:{xs:'calc(100vw)', md:'auto'}}}>
+                <Fade in={true}>
+                    <Paper sx={{padding:'1rem'}}>
+                        <Content content={markdown} state={state}/>
+                    </Paper>
+                </Fade>
+            </Container>
+        </MainPage>
+    )
+}
---
+++ src/app/sitemap.ts
@@ -0,0 +1,44 @@
+import { SITE_CONFIG } from '@/config';
+import { categories, chapters, docs, docState, languages } from '@/utils/document';
+import type { MetadataRoute } from 'next'
+
+export const dynamic = 'force-static'
+
+export default async function sitemap():Promise<MetadataRoute.Sitemap> {
+    const sites = [];
+
+    // 根路径
+    const base = new URL(SITE_CONFIG.basePath, SITE_CONFIG.origin);
+    const state = await docState('zh', '00.index.md');
+    sites.push({
+        url: base.toString(),
+        lastModified: state.updatedTime
+    });
+
+    for (const lang of await languages()) {
+        const state = await docState(lang, '00.index.md');
+        sites.push({
+            url: new URL(`${SITE_CONFIG.basePath}/document/${lang}`, SITE_CONFIG.origin).toString(),
+            lastModified: state.updatedTime
+        });
+
+        for (const category of await categories(lang)) {
+            const state = await docState(lang, category, '00.index.md');
+            sites.push({
+                url: new URL(`${SITE_CONFIG.basePath}/document/${lang}/${category}`, SITE_CONFIG.origin).toString(),
+                lastModified: state.updatedTime
+            });
+
+            for (const chapter of await chapters(lang, category)) {
+                for (const doc of await docs(lang, category, chapter)) {
+                    const state = await docState(lang, category, chapter, doc);
+                    sites.push({
+                        url: new URL(`${SITE_CONFIG.basePath}/document/${lang}/${category}/${chapter}/${doc}`, SITE_CONFIG.origin).toString(),
+                        lastModified: state.updatedTime
+                    });
+                }
+            }
+        }
+    }
+    return sites;
+}
