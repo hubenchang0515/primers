e95b5abb8548f3cb4f9860d045049596c0417873
PlanC <hubenchang0515@outlook.com>
2025-05-17 13:35:33 +0000

    减少首屏布局偏移

--- src/components/GlobalState.tsx
+++ src/components/GlobalState.tsx
@@ -1,18 +1,19 @@
 'use client';
+import theme from '@/utils/theme';
 import React, { createContext, useContext, useEffect, useState } from 'react';
 
 export interface GlobalStateProps {
+    loading?: boolean;
+    setLoading?: (v?:boolean)=>void;
+
     currentTitle?: number;
     expandedSideGroup?: number;
     sideExpanded?: boolean;
     sideCollapsedSize?: number;
-    loading?: boolean;
-
     setCurrentTitle?: (v?:number)=>void;
     setExpandedSideGroup?: (v?:number)=>void;
     setSideExpanded?: (v?:boolean)=>void;
     setSideCollapsedSize?: (v?:number)=>void;
-    setLoading?: (v?:boolean)=>void;
 }
 
 export const GlobalState = createContext<GlobalStateProps>({});
@@ -26,16 +27,19 @@ export interface GlobalStateProviderProps {
 }
 
 export function GlobalStateProvider(props:GlobalStateProviderProps) {
+    const [loading, setLoading] = useState<boolean|undefined>(true);
+
     const [currentTitle, setCurrentTitle] = useState<number|undefined>(undefined);
     const [expandedSideGroup, setExpandedSideGroup] = useState<number|undefined>(undefined);
-    const [sideExpanded, setSideExpanded] = useState<boolean|undefined>(false);
-    const [sideCollapsedSize, setSideCollapsedSize] = useState<number|undefined>(56);   // 16 + 24 + 16 => 56 
-    const [loading, setLoading] = useState<boolean|undefined>(false);   // 16 + 24 + 16 => 56 
+    const [sideExpanded, setSideExpanded] = useState<boolean|undefined>(true);
+    const [sideCollapsedSize, setSideCollapsedSize] = useState<number|undefined>(0);   // 16 + 24 + 16 => 56 
 
     useEffect(() => {
-        if (window.innerWidth < 900) {
+        if (window.innerWidth < theme.breakpoints.values.md) {
             setSideCollapsedSize?.(0);
+            setSideExpanded?.(false);
         } else {
+            setSideCollapsedSize?.(56);
             setSideExpanded?.(true);
         }
     }, [setSideCollapsedSize]);
@@ -43,16 +47,17 @@ export function GlobalStateProvider(props:GlobalStateProviderProps) {
     return (
         <GlobalState.Provider 
             value={{
+                loading,
+                setLoading,
+
                 currentTitle, 
                 expandedSideGroup, 
                 sideExpanded,
                 sideCollapsedSize,
-                loading,
                 setCurrentTitle,
                 setExpandedSideGroup,
                 setSideExpanded,
                 setSideCollapsedSize,
-                setLoading,
             }}
         >
             { props.children }
