ec358d037fd26b157151b309027f2dd8e7b85b2d
PlanC <hubenchang0515@outlook.com>
2025-05-20 12:59:09 +0000

    iframe 初始化高度

--- src/components/EmbedPage.tsx
+++ src/components/EmbedPage.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { Box, Button, Collapse } from "@mui/material";
 import DOMPurify from "isomorphic-dompurify";
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useGlobalState } from "./GlobalState";
 
 export interface EmbedPageProps {
     code: string;
@@ -12,13 +13,20 @@ export interface EmbedPageProps {
 
 export default function EmbedPage(props:EmbedPageProps) {
     const ref = useRef<HTMLIFrameElement>(null);
-    const [expanded, setExpanded] = useState(false);
+
+    const {
+        loading,
+    } = useGlobalState();
 
     const initHeight = useCallback(() => {
         if (ref.current?.contentWindow) {
             ref.current.style.height = ref.current.contentWindow.document.documentElement.scrollHeight + 'px';
         }
     }, [ref]);
+
+    useEffect(() => {
+        initHeight();
+    }, [loading]);
 
     return (
         <Box sx={{width:'100%', boxSizing:'border-box', }}>
@@ -27,17 +35,25 @@ export default function EmbedPage(props:EmbedPageProps) {
                     sx={{position:'absolute', boxSizing:'border-box', top:0, bottom:0, width:'100%', overflow:'auto'}} 
                     dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(props.code)}}
                 />
-                <Collapse in={expanded} collapsedSize={150}>
-                    <iframe 
-                        ref={ref}
-                        srcDoc={props.unsafe ? props.code : DOMPurify.sanitize(props.code, {WHOLE_DOCUMENT: true, ADD_ATTR:['id']})}
-                        style={{width:'100%', height:'100%', background:'#fafafa', position:'relative', zIndex:1, minHeight:0, boxSizing:'border-box', border:0, margin:0, lineHeight:0, overflow:'hidden'}}
-                    />
-                </Collapse>
+                <iframe 
+                    ref={ref}
+                    srcDoc={props.unsafe ? props.code : DOMPurify.sanitize(props.code, {WHOLE_DOCUMENT: true, ADD_ATTR:['id']})}
+                    style={{
+                        width:'100%', 
+                        height: '100%', 
+                        background:'#fafafa', 
+                        position:'relative', 
+                        zIndex:1, 
+                        minHeight:0, 
+                        boxSizing:'border-box', 
+                        border:0, 
+                        margin:0, 
+                        lineHeight:0, 
+                        overflow:'hidden'
+                    }}
+                    onLoad={initHeight}
+                />
             </Box>
-            <Button variant="contained" color="secondary" size="small" sx={{margin:'auto',borderRadius:0}} onClick={()=>{initHeight(); setExpanded(!expanded);}}>
-                {expanded ? <ExpandLessIcon/> : <ExpandMoreIcon/>}
-            </Button>
         </Box>
     )
 }
