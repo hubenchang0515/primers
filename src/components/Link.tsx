54efb90ef6d18b1ca939e8c68cbbd56d7bf43fbb
PlanC <hubenchang0515@outlook.com>
2025-07-26 03:07:16 +0000

    修复内链打开新标签页的问题

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -3,6 +3,7 @@ import {Link as MuiLink, LinkProps as MuiLinkProps } from '@mui/material';
 import NextLink from "next/link";
 import React, { MouseEvent } from 'react';
 import { useGlobalState } from './GlobalState';
+import { SITE_CONFIG } from '@/config';
 
 export default function Link(props:MuiLinkProps) {
     const { setLoading } = useGlobalState();
@@ -11,7 +12,7 @@ export default function Link(props:MuiLinkProps) {
         return <MuiLink {...props} color='textDisabled' href="#">{props.children}</MuiLink>
     }
 
-    if (props.href.startsWith("http")) {
+    if (props.href.startsWith("http") && !props.href.startsWith(SITE_CONFIG.origin + SITE_CONFIG.basePath)) {
         return <MuiLink component={NextLink} target="_blank" rel="noopener" {...props}>{props.children}</MuiLink>
     }
     
