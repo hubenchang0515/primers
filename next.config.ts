a596913a0edcdd0a3b75d7d2a5d2e32c1cdaa781
PlanC <hubenchang0515@outlook.com>
2025-09-10 15:52:28 +0000

    延长允许的构建时间

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
   basePath: SITE_CONFIG.basePath,
   assetPrefix: SITE_CONFIG.basePath,
   distDir: `out/${SITE_CONFIG.basePath}`,
+  staticPageGenerationTimeout: 180,
   output: 'export',
 };
 
