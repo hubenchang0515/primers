b70a60715af665218ffb2701fba155ecbd3947dd
PlanC <hubenchang0515@outlook.com>
2025-10-30 16:49:18 +0000

    404 页面添加 noindex

--- src/app/empty/page.tsx
+++ src/app/empty/page.tsx
@@ -1,6 +1,21 @@
+
+import { Metadata } from "next";
 import NotFound from "@/components/NotFound";
 import { searchTree } from "@/utils/search";
 import { Suspense } from "react";
+import { SITE_CONFIG } from "@/config";
+
+// 生成元数据
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        title: `404 Not Found - ${SITE_CONFIG.title}`,
+        robots: "noindex",
+        icons: {
+            icon: `${SITE_CONFIG.basePath}/favicon.svg`,
+        },
+        
+    };
+}
 
 // 生成页面
 export default async function NotFoundPage() {
