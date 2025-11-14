2d5678ce685547cf74fea596b5cfebc60b20fe16
PlanC <hubenchang0515@outlook.com>
2025-11-14 07:12:18 +0000

    mermaid 渲染优化

--- src/utils/mermaid.ts
+++ src/utils/mermaid.ts
@@ -1,26 +1,29 @@
-import { execFile } from "child_process";
+import { spawn } from "child_process";
 
 export async function render(code:string) {
     return new Promise<string>((resolve, reject) => {
-        const process = execFile('mmdc', 
-            [
-                '-p', 'puppeteer-config.json', 
-                '-c', 'mermaid-config.json', 
-                '-b', 'transparent',
-                '-e', 'svg',
-                '--output', '-',    // 输出到 stdout
-                '--input', '-'      // 从 stdin 读取输入
-            ], 
-            async (err, stdout, stderr) => {
-                if (err) {
-                    console.error(err, stderr);
-                    reject(stderr.trim());
-                } else {
-                    resolve(stdout.trim());
-                }
-            }
-        );
+        const process = spawn('mmdc',[
+            '-p', 'puppeteer-config.json', 
+            '-c', 'mermaid-config.json', 
+            '-b', 'transparent',
+            '-e', 'svg',
+            '--output', '-',    // 输出到 stdout
+            '--input', '-'      // 从 stdin 读取输入
+        ])
+
+        let stdout = "";
+        let stderr = "";
         
+        process.stdout.on("data", (d) => stdout += d);
+        process.stderr.on("data", (d) => stderr += d);
+        process.on("close", (result) => {
+            if (result === 0) {
+                resolve(stdout)
+            } else {
+                reject(stderr)
+            }
+        })
+
         process.stdin?.write(code);
         process.stdin?.end(); 
     });
