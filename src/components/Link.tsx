6cba28de8a4c06a21954f0255b799350890e6699
PlanC <hubenchang0515@outlook.com>
2026-01-19 15:00:33 +0000

    修复点击当前页面的链接，会一直显示加载中的问题

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -15,12 +15,22 @@ export default function Link(props:MuiLinkProps) {
     if (props.href.startsWith("http") && !props.href.startsWith(SITE_CONFIG.origin + SITE_CONFIG.basePath)) {
         return <MuiLink component={NextLink} target="_blank" rel="noopener" {...props}>{props.children}</MuiLink>
     }
+
+    if (props.href.startsWith("#")) {
+        return <MuiLink component={NextLink} {...props}>{props.children}</MuiLink>
+    }
     
     return (
         <MuiLink
             component={NextLink}
             {...props}
-            onClick={(ev:MouseEvent<HTMLAnchorElement>)=>{setLoading?.(true); props.onClick?.(ev)}}
+            onClick={(ev:MouseEvent<HTMLAnchorElement>)=>{
+                const target = new URL(SITE_CONFIG.basePath + (props.href??''), SITE_CONFIG.origin);
+                if (target.pathname !== window.location.pathname) {
+                    setLoading?.(true);
+                }
+                props.onClick?.(ev);
+            }}
         >
             {props.children}
         </MuiLink>
