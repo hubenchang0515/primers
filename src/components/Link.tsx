1a799dac0811e089512b58648d8cfc7b11dbaeb2
PlanC <hubenchang0515@outlook.com>
2026-02-20 08:13:05 +0000

    修复点击当前页面锚点链接时显示加载动画的问题

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,25 +1,36 @@
 "use client";
 import {Link as MuiLink, LinkProps as MuiLinkProps } from '@mui/material';
 import NextLink from "next/link";
-import React, { MouseEvent } from 'react';
+import { MouseEvent, useEffect, useState } from 'react';
 import { useGlobalState } from './GlobalState';
 import { SITE_CONFIG } from '@/config';
 
 export default function Link(props:MuiLinkProps) {
     const { setLoading } = useGlobalState();
+    const [ location, setLocation ] = useState<Location>();
 
+    useEffect(() => {
+        setLocation(window.location);
+    }, []);
+
+    // 空链接
     if (!props.href) {
         return <MuiLink {...props} color='textDisabled' href="#">{props.children}</MuiLink>
     }
 
+    // 外部链接
     if (props.href.startsWith("http") && !props.href.startsWith(SITE_CONFIG.origin + SITE_CONFIG.basePath)) {
         return <MuiLink component={NextLink} target="_blank" rel="noopener" {...props}>{props.children}</MuiLink>
     }
 
-    if (props.href.startsWith("#")) {
+    // 当前页面
+    if (props.href.startsWith("#") || 
+        location && props.href.startsWith(location.pathname + '#') || 
+        location && props.href.startsWith(location.origin + location.pathname + '#')) {
         return <MuiLink component={NextLink} {...props}>{props.children}</MuiLink>
     }
     
+    // 内部链接
     return (
         <MuiLink
             component={NextLink}
