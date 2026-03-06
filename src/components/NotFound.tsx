3396659fb8f7fedf72fa072ee4fa92dc840da9c8
PlanC <hubenchang0515@outlook.com>
2026-03-06 22:06:26 +0000

    404 页面修复相似分的计算错误

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -8,6 +8,7 @@ import { SearchNode } from "@/utils/search";
 import Link from "./Link";
 import { like, title } from "@/utils/text";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 interface SimilarNode {
     url: string,
@@ -19,38 +20,30 @@ interface SimilarNode {
 function findSimilar(root:SearchNode, lang:string, category:string, chapter: string, doc:string) {
     const similar:SimilarNode[] = [];
     for (const langNode of root.children??[]) {
-        let rank = 0;
-        
-        if (like(langNode.title, title(lang))) {
-            rank += 1;
-        }
+        const rank1 = like(langNode.title, title(lang)) ? 1 : 0;
 
         if (like(langNode.title, title(lang)) && !category) {
-            similar.push({...langNode, rank: rank + 1});
+            similar.push({...langNode, rank: rank1 + 1});
             break;
         }
 
         for (const categoryNode of langNode.children??[]) {
-            if (like(categoryNode.title, title(category))) {
-                rank += 2;
-            }
+            const rank2 = rank1 + (like(categoryNode.title, title(category)) ? 2 : 0);
 
             if (like(categoryNode.title, title(category)) && !chapter) {
-                similar.push({...categoryNode, rank: rank + 2});
+                similar.push({...categoryNode, rank: rank2 + 2});
                 break;
             }
 
             for (const chapterNode of categoryNode.children??[]) {
-                if (like(chapterNode.title, title(chapter))) {
-                    rank += 3;
-                }
+                const rank3 = rank2 + (like(chapterNode.title, title(chapter)) ? 3 : 0);
 
                 for (const docNode of chapterNode.children??[]) {
                     if (!like(docNode.title, title(doc))) {
                         continue;
                     }
 
-                    similar.push({...docNode, text: `${categoryNode.title} -> ${chapterNode.title} -> ${docNode.title}`, rank: rank + 4});
+                    similar.push({...docNode, text: `${categoryNode.title} -> ${chapterNode.title} -> ${docNode.title}`, rank: rank3 + 4});
                 }
             }
         }
@@ -73,17 +66,24 @@ export default function NotFound(props:NotFoundProps) {
     const doc = searchParams.get('doc')??''.trim();
     const i18n = new I18n(lang); 
     const similar = findSimilar(props.root, lang, category, chapter, doc);
-
-    // 如果 rank 是 10 分，说明路径标题完全一致，直接跳转
     const router = useRouter();
-    if (similar[0]?.rank === 10) {
-        router.replace(similar[0].url);
-    }
+
+    useEffect(() => {
+        if (similar[0]?.rank === 10) {
+            router.replace(similar[0].url);
+        }
+    }, [similar, router]);
     
     return (
         <MainPage lang={lang} depth={1} disableDiscussion>
             <Paper sx={{flex:1}}>
-                <Typography variant="h1" align="center" sx={{fontSize:'clamp(1.25rem, 6vw, 2.5rem)', fontWeight:'bolder', marginBlock:'1rem'}}>404 Not Found</Typography>
+                <Typography variant="h1" align="center" sx={{fontSize:'clamp(1.25rem, 6vw, 2.5rem)', fontWeight:'bolder', marginBlock:'1rem'}}>
+                    {
+                        similar[0]?.rank === 10 ?
+                        <>Waiting...</> :
+                        <>404 Not Found</>
+                    }
+                </Typography>
                 <Alert severity="warning">
                     <span style={{wordBreak:'break-all'}}>{i18n.t('notfound.alert')}</span>
                     <span style={{color:'red', textDecoration:'underline', wordBreak:'break-all'}}>/{[lang,category,chapter,doc].filter(Boolean).join('/')}</span>
