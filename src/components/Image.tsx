a1d39049f02727bb538303a421dd40ea709a134e
planc <hubenchang0515@outlook.com>
2026-09-21 15:25:29 +0800

    支持外部图片，且可以设置图片大小

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -6,14 +6,33 @@ export interface ImageProps {
     src?: string;
     absolute?: boolean;
     alt?: string;
+    title?: string;
     style?: CSSProperties;
 }
 
 export default async function Image(props:ImageProps) {
+    const size = {width:0, height: 0};
+    const match = props.alt?.match(/.+!size:(\d+)x(\d+)/);
+    if (match) {
+        size.width = Number(match[1]);
+        size.height = Number(match[2]);
+    }
+    if (props.src?.startsWith("http")) {
+        <img src={props.src} width={size.width > 0 ? size.width : undefined} height={size.height > 0 ? size.height : undefined}/>
+    }
+
     try {
         const info = await imageInfo(props.src??"", props.absolute);
         return (
-            <NextImage src={info.src} alt={props.alt??'image'} width={info.width} height={info.height} style={props.style} rel="nofollow"/>
+            <NextImage 
+                src={info.src} 
+                alt={props.alt??'image'} 
+                title={props.title} 
+                width={size.width > 0 ? size.width : info.width} 
+                height={size.height > 0 ? size.height : info.height} 
+                style={props.style}
+                rel="nofollow"
+            />
         )
     } catch {
         return <NextImage src='/404' width={160} height={90} alt={props.alt??'image'} style={props.style}/>
